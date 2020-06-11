25b53ac8f9d3ea5ff46178ea0c1d185d51c78ba1
Pablok95 <kotoniak@gmail.com>
2020-06-11 20:05:30 +0200

    feat(): Styling location section

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -5,19 +5,19 @@ const Footer = () => (
   <footer className="footer">
     <div className="container">
       <div className="footer__content">
-        <p className="text text--bold">Copyright &copy; {new Date().getFullYear()}</p>
+        <p className="text text--bold">Copyright &copy; {new Date().getFullYear()} Wszelkie prawa zastrzeżone</p>
         <p className="text text--bold">
           Wykonane przez: <span className="text text--accent">
           <Link
             to="/"
             target="_blank"
-            className="link link--active"
+            className="link link--active link--white"
           >Wróbel</Link>&
           <Link
             to="https://www.linkedin.com/in/kotoniak/"
             target="_blank"
             title="See linkedin profile"
-            className="link link--active"
+            className="link link--active link--white"
           >Kotoniak</Link>
         </span></p>
       </div>
--- src/templates/locations-section.js
+++ src/templates/locations-section.js
@@ -1,9 +1,34 @@
 import React from "react"
 
 const LocationSection = () => (
-  <section>
+  <section className="cmt-xsm-4 cmt-md-6 cmt-8 cmb-xsm-4 cmb-md-6 cmb-8">
     <div className="container">
-      Lokalizacja
+      <div className="location">
+        <h2 className="section-header section-header--offset">Lokalizacja</h2>
+
+        <div className="row">
+          <div className="col-12 col-md-7 col-xl-8">
+            <h3 className="location__header">Centrum Stolarki Okiennej <span className="location__header--accent">Krzysztof Burda</span>
+            </h3>
+            <p className="text">32-400 Myślenice, ul. 3-go Maja 14c</p>
+            <p className="text">tel./fax <a href="tel:+48122741003" title="tel./fax: +48 12 274 10 03"
+                                            className="link link--white">+48 12 274 10 03</a></p>
+            <p className="text">tel. kom. <a href="tel:+48601530589" title="tel./kom: +48 601 530 589"
+                                             className="link link--white">+48 601 530 589</a></p>
+            <a href="mailto:centrumstolarki@okna-drzwi-myslenice.pl"
+               title="email: centrumstolarki@okna-drzwi-myslenice.pl"
+               className="link link--white">centrumstolarki@okna-drzwi-myslenice.pl</a>
+          </div>
+          <div className="col-12 col-md-5 col-xl-4">
+            <p className="text cmt-3">Bank: BPH S.A. Oddział Myślenice</p>
+            <p className="text">Numer konta: 51051515055151515</p>
+            <p className="text">NIP: 681-142-26-43 &#9899; REGON: 356291852</p>
+            <a href="https://goo.gl/maps/ffA3Egv2dYMQ5urv5" target="_blank" title="Zobacz mapę dojazdu"
+               className="link link--accent link--white cmt-2">Zobacz mapę
+              dojazdu &#8594;</a>
+          </div>
+        </div>
+      </div>
     </div>
   </section>
 )
